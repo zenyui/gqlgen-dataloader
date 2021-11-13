eaa66dc12876dd173fd6cd03a5526f90e39ce633
zen <zenyui@gmail.com>
2021-11-13 15:06:41 -0500

    single loader

--- graph/dataloader/dataloader.go
+++ graph/dataloader/dataloader.go
@@ -47,10 +47,10 @@ func NewDataLoader(db storage.Storage) *DataLoader {
 
 // Middleware injects a DataLoader into the request context so it can be
 // used later in the schema resolvers
-func Middleware(db storage.Storage, next http.Handler) http.Handler {
-	loaders := NewDataLoader(db)
+func Middleware(loader *DataLoader, next http.Handler) http.Handler {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		nextCtx := context.WithValue(r.Context(), loadersKey, loaders)
+		nextCtx := context.WithValue(r.Context(), loadersKey, loader)
 		r = r.WithContext(nextCtx)
 		next.ServeHTTP(w, r)
 	})
